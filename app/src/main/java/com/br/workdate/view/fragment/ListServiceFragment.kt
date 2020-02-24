4576cfd81e2ca9970b85c980f2b0f0ca17d98839
Newton Cesar <new123cesar@gmail.com>
2020-02-24 00:47:59 -0300

    refact Choose Service

--- app/src/main/java/com/br/workdate/view/fragment/ListServiceFragment.kt
+++ app/src/main/java/com/br/workdate/view/fragment/ListServiceFragment.kt
@@ -1,56 +1,23 @@
 package com.br.workdate.view.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.br.workdate.R
 import com.br.workdate.model.Service
 import com.br.workdate.view.dialog.ServiceFormInsertDialog
 import com.br.workdate.view.dialog.ServiceFormUpdateDialog
-import com.br.workdate.view.recyclerview.adapter.ServiceAdapter
-import com.br.workdate.view.viewmodel.ServiceViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_list_service.*
-import org.koin.android.viewmodel.ext.android.viewModel
 
-class ListServiceFragment : Fragment() {
+class ListServiceFragment : BaseListServiceFragment() {
 
-    private val viewModel: ServiceViewModel by viewModel()
+    override fun implementIfWannaHideFab(newService: FloatingActionButton) {}
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list_service, container, false)
-    }
+    override fun doInFabClickListener() = callInsertDialog()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        service_list_animation.setAnimation("anim/client_empty.json")
+    override fun doInItemClickListener(service: Service) = callUpdateDialog(service)
 
-        new_service.setOnClickListener {
-            callInsertDialog()
-        }
-        viewModel.listAll().observe(viewLifecycleOwner, androidx.lifecycle.Observer { serviceList ->
-            ifEmptyPlayAnimation(serviceList)
-            initServiceAdapter(serviceList)
-        })
-    }
-
-    private fun initServiceAdapter(services: MutableList<Service>) {
-        val adapter = context?.let { context -> ServiceAdapter(context, services) }
-        service_list_rv.adapter = adapter
-        adapter!!.onItemClickListener = { service ->
-            callUpdateDialog(service)
-        }
-        adapter.onItemLongClickListener = { service ->
-            viewModel.remove(service)
-            showSnackBar(service, "removed")
-        }
+    override fun doInItemLongClickListener(service: Service) {
+        super.viewModel.remove(service)
+        showSnackBar(service, "removed")
     }
 
     private fun callInsertDialog() {
@@ -76,23 +43,6 @@ class ListServiceFragment : Fragment() {
     private fun showSnackBar(service: Service, msg: String) {
         view?.let { view ->
             Snackbar.make(view, "${service.description} " + msg, Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun ifEmptyPlayAnimation(mutableList: MutableList<Service>) {
-        if (mutableList.isEmpty()) {
-            initAnimation()
-        } else {
-            service_list_animation.visibility = GONE
-        }
-    }
-
-    private fun initAnimation() {
-        with(service_list_animation) {
-            scaleX = 0.5f
-            scaleY = 0.5f
-            visibility = VISIBLE
-            playAnimation()
         }
     }
 
--- app/src/main/java/com/br/workdate/view/fragment/SearchListServiceFragment.kt
+++ app/src/main/java/com/br/workdate/view/fragment/SearchListServiceFragment.kt
@@ -1,63 +1,19 @@
 package com.br.workdate.view.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.br.workdate.R
 import com.br.workdate.model.Service
-import com.br.workdate.view.recyclerview.adapter.ServiceAdapter
-import com.br.workdate.view.viewmodel.ServiceViewModel
-import kotlinx.android.synthetic.main.fragment_list_service.*
-import org.koin.android.viewmodel.ext.android.viewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class SearchListServiceFragment : Fragment() {
+class SearchListServiceFragment : BaseListServiceFragment() {
 
-    private val viewModel: ServiceViewModel by viewModel()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list_service, container, false)
+    override fun implementIfWannaHideFab(newService: FloatingActionButton) {
+        newService.hide()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        service_list_animation.setAnimation("anim/client_empty.json")
+    override fun doInFabClickListener() {}
 
-        new_service.setOnClickListener {}
-
-        viewModel.listAll().observe(viewLifecycleOwner, androidx.lifecycle.Observer { serviceList ->
-            ifEmptyPlayAnimation(serviceList)
-            initServiceAdapter(serviceList)
-        })
+    override fun doInItemClickListener(service: Service) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private fun initServiceAdapter(services: MutableList<Service>) {
-        val adapter = context?.let { context -> ServiceAdapter(context, services) }
-        service_list_rv.adapter = adapter
-        adapter!!.onItemClickListener = {}
-    }
-
-    private fun ifEmptyPlayAnimation(mutableList: MutableList<Service>) {
-        if (mutableList.isEmpty()) {
-            initAnimation()
-        } else {
-            service_list_animation.visibility = GONE
-        }
-    }
-
-    private fun initAnimation() {
-        with(service_list_animation) {
-            scaleX = 0.5f
-            scaleY = 0.5f
-            visibility = VISIBLE
-            playAnimation()
-        }
-    }
-
+    override fun doInItemLongClickListener(service: Service) {}
 }

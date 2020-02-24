0f86f4857f609a3a6fb559db81287afa54c28871
Newton Cesar <new123cesar@gmail.com>
2020-02-23 23:50:59 -0300

    implemented choose client and service inf FormSchedule

--- app/src/main/java/com/br/workdate/view/fragment/FormScheduleFragment.kt
+++ app/src/main/java/com/br/workdate/view/fragment/FormScheduleFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.br.workdate.R
 import com.br.workdate.view.viewmodel.StateAppComponentsViewModel
 import com.br.workdate.view.viewmodel.VisualComponents
+import kotlinx.android.synthetic.main.fragment_form_schedule.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class FormScheduleFragment : Fragment() {
 
     private val appComponentsViewModel: StateAppComponentsViewModel by sharedViewModel()
+    private val navController by lazy { NavHostFragment.findNavController(this) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +26,29 @@ class FormScheduleFragment : Fragment() {
 
         appComponentsViewModel.havCoponent = VisualComponents(false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        form_schedule_client_btn.setOnClickListener {
+            goToSearchClientFragment()
+        }
+
+        form_schedule_service_btn.setOnClickListener {
+            goToSearchServiceFragment()
+        }
+    }
+
+    private fun goToSearchServiceFragment() {
+        val direction = FormScheduleFragmentDirections
+            .actionFormScheduleFragmentToSearchListServiceFragment()
+        navController.navigate(direction)
+    }
+
+    private fun goToSearchClientFragment() {
+        val direction = FormScheduleFragmentDirections
+            .actionFormScheduleFragmentToSearchListClientFragment()
+        navController.navigate(direction)
     }
 
 }

cf55f4fd8eb641ba7daafea3f38fc46b0b8f450e
Newton Cesar <new123cesar@gmail.com>
2020-11-11 22:42:03 -0300

    refact

--- app/src/main/java/com/br/workdate/view/databinding/ServiceData.kt
+++ app/src/main/java/com/br/workdate/view/databinding/ServiceData.kt
@@ -4,15 +4,18 @@ import androidx.lifecycle.MutableLiveData
 import com.br.workdate.extension.returnUUID
 import com.br.workdate.extension.tryParseBigDecimal
 import com.br.workdate.model.Service
+import java.math.BigDecimal
 
 class ServiceData(
     private var service: Service,
+    private val emptyString: String = "",
     val id: MutableLiveData<String> = MutableLiveData<String>().also { it.value = service.id },
     val description: MutableLiveData<String> = MutableLiveData<String>().also {
         it.value = service.description
     },
     val value: MutableLiveData<String> = MutableLiveData<String>().also {
-        it.value = service.value.toString()
+        @Suppress("UNUSED_EXPRESSION")
+        if(service.value != BigDecimal.ZERO) it.value = service.value.toString() else emptyString
     },
 ) {
 

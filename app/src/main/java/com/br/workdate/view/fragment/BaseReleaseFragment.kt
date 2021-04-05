0266a2b9f6abf5f30cce7a9440d790726fb50c35
NewtonCesarRoncari <new123cesar@gmail.com>
2021-04-05 14:19:42 -0300

    refact

--- app/src/main/java/com/br/workdate/view/fragment/BaseReleaseFragment.kt
+++ app/src/main/java/com/br/workdate/view/fragment/BaseReleaseFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.lottie.LottieAnimationView
 import com.br.workdate.model.Release
+import com.br.workdate.model.Situation
 import com.br.workdate.view.recyclerview.adapter.ReleaseAdapter
+import com.br.workdate.view.viewmodel.ReleaseViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
 open class BaseReleaseFragment : Fragment() {
 
-    protected fun initAdapter(releases: MutableList<Release>, recyclerView: RecyclerView) {
+    private val viewModel: ReleaseViewModel by viewModel()
+
+    private fun initAdapter(releases: MutableList<Release>, recyclerView: RecyclerView) {
         val adapter = context?.let { ReleaseAdapter(it, releases) }
         recyclerView.adapter = adapter
     }
 
-    protected fun ifEmptyPlayAnimation(
+    private fun ifEmptyPlayAnimation(
         mutableList: MutableList<Release>,
         listAnimation: LottieAnimationView
     ) {
@@ -32,5 +37,31 @@ open class BaseReleaseFragment : Fragment() {
             visibility = View.VISIBLE
             playAnimation()
         }
+    }
+
+    protected fun listAll(
+        situation: Situation,
+        listAnimation: LottieAnimationView,
+        recyclerView: RecyclerView
+    ) {
+        viewModel.listAll(situation).observe(viewLifecycleOwner, { releases ->
+            ifEmptyPlayAnimation(releases, listAnimation)
+            initAdapter(releases, recyclerView)
+        })
+    }
+
+    protected fun checkReleasesReturned(
+        situation: Situation,
+        listAnimation: LottieAnimationView,
+        recyclerView: RecyclerView
+    ) {
+        viewModel.checkReleasesReturned()?.observe(viewLifecycleOwner, { releases ->
+            if (releases != null) {
+                ifEmptyPlayAnimation(releases, listAnimation)
+                initAdapter(releases.filter { release ->
+                    release.situation == situation
+                } as MutableList<Release>, recyclerView)
+            }
+        })
     }
 }
--- app/src/main/java/com/br/workdate/view/fragment/PaidReleaseFragment.kt
+++ app/src/main/java/com/br/workdate/view/fragment/PaidReleaseFragment.kt
@@ -4,42 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.br.workdate.R
-import com.br.workdate.model.Release
+import com.br.workdate.databinding.FragmentExpenseBinding
 import com.br.workdate.model.Situation
-import com.br.workdate.view.viewmodel.ReleaseViewModel
-import kotlinx.android.synthetic.main.fragment_expense.*
-import org.koin.android.viewmodel.ext.android.viewModel
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 open class PaidReleaseFragment : BaseReleaseFragment() {
 
-    private val viewModel: ReleaseViewModel by viewModel()
+    private val binding by viewBinding(FragmentExpenseBinding::bind)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_expense, container, false)
+        return FragmentExpenseBinding.inflate(inflater).root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        expense_list_animation.setAnimation("anim/list_empty.json")
+        binding.expenseListAnimation.setAnimation("anim/list_empty.json")
 
-        viewModel.listAll(Situation.PAID).observe(viewLifecycleOwner, { releases ->
-            super.ifEmptyPlayAnimation(releases, expense_list_animation)
-            super.initAdapter(releases, expenses_rv)
-        })
-
-        viewModel.checkReleasesReturned()?.observe(viewLifecycleOwner, { releases ->
-            if (releases != null) {
-                releases.filter { release -> release.situation == Situation.PAID }
-                super.ifEmptyPlayAnimation(releases, expense_list_animation)
-                super.initAdapter(releases.filter { release ->
-                    release.situation == Situation.PAID
-                } as MutableList<Release>, expenses_rv)
-            }
-        })
+        super.listAll(Situation.PAID, binding.expenseListAnimation, binding.expensesRv)
+        super.checkReleasesReturned(
+            Situation.PAID,
+            binding.expenseListAnimation,
+            binding.expensesRv
+        )
     }
 
 }
